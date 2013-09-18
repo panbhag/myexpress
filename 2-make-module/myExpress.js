6e13a7708790bd3ecea605f6c94d9f978041cb9a
pankajbhageria <pankajbhageria@gmail.com>
2013-09-18 10:03:11 +0530

    add headers

--- 2-make-module/myExpress.js
+++ 2-make-module/myExpress.js
@@ -4,8 +4,11 @@ function myExpress()
 {
 
    var server = http.createServer(function(req, res) {
-        res.writeHead(200);
-        res.end('Hello Http from module');
+       var message = 'Hello Http from module'
+       res.setHeader('Content-Type', 'text/plain');
+       res.setHeader('Content-Length', message.length);
+       res.writeHead(200);
+       res.end(message);
     });
 
    
